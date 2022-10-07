3ffcc416e9d898173d391228fd771567060008b2
micahweiss25 <34974760+micahweiss25@users.noreply.github.com>
2022-10-06 22:02:35 -0400

    We plan to use scala pattern matching to parse the tree and then evaluate

--- hw4.scala
+++ hw4.scala
@@ -5,7 +5,7 @@ object hw4 extends eecs.cs478:
 
   /*
   If working by yourself, delete this comment block; otherwise, fill it out.
-    Contribution of <insert team member 1's name here>: <team member 1's contribution>
+    Contribution of Suess: has created context free grammar that will be used for pattern matching 
     Contribution of <insert team member 2's name here>: <team member 2's contribution>
     ...
     Contribution of <insert team member n's name here>: <team member n's contribution>
